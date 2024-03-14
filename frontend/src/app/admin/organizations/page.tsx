223cd8674c9446e8adf2352630ff479179d23c3d
asofianidis <ansofianidis@outlook.com>
2024-03-13 20:40:37 -0500

    created organizations type, and added the table for organizations

--- frontend/src/app/admin/organizations/page.tsx
+++ frontend/src/app/admin/organizations/page.tsx
@@ -7,10 +7,33 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Organizations } from "@/types/organizations";
 
-async function GetOrganizations(){}
+async function GetOrganizations(): Promise<Organizations[]> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/Organizations/GetAllOrgs`,
+      { cache: "no-cache" }
+    );
 
-export default function OrganizationPage() {
+    if (!res.ok) {
+      throw new Error(
+        `Failed to fetch organizations, status code ${res.status}`
+      );
+    }
+
+    const data = await res.json();
+
+    return Array.isArray(data) ? data : [];
+  } catch (e: any) {
+    console.log(e);
+    return [];
+  }
+}
+
+export default async function OrganizationPage() {
+  const organizations = await GetOrganizations();
+
   return (
     <div className="mt-8 mx-6 mb-6 flex flex-col gap-5">
       <h2 className="text-2xl font-bold">Organizations</h2>
@@ -18,12 +41,21 @@ export default function OrganizationPage() {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead>ID</TableHead>
+            <TableHead className="font-bold text-black">ID</TableHead>
             <TableHead>Name</TableHead>
             <TableHead></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
+          {organizations.map((o) => {
+            return (
+              <TableRow>
+                <TableCell>{o.org_id}</TableCell>
+                <TableCell>{o.name}</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </div>
---
+++ frontend/src/types/organizations.ts
@@ -0,0 +1,4 @@
+export type Organizations = {
+    org_id: number
+    name: string
+}
