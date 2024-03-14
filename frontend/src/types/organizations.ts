5f372e72ed10bdda349e59fac69b2654b58179cb
asofianidis <ansofianidis@outlook.com>
2024-03-13 21:09:19 -0500

    added body type

--- frontend/src/types/organizations.ts
+++ frontend/src/types/organizations.ts
@@ -1,4 +1,10 @@
 export type Organizations = {
-    org_id: number
-    name: string
-}
+  org_id: number;
+  name: string;
+};
+
+export type OrganizationBody = {
+  org_id?: number;
+  organizationName?: string;
+  org_name?: string;
+};
