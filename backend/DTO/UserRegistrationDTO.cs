92c75a4b94edd5d4294250af4c84ca05dfa3b71e
asofianidis <ansofianidis@outlook.com>
2024-03-19 21:12:12 -0500

    created user registration dto

---
+++ backend/DTO/UserRegistrationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.DTO
+{
+    public class UserRegistrationDTO
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string org_code { get; set; }
+        public string email { get; set; }
+        public string full_name { get; set; }
+    }
+}
