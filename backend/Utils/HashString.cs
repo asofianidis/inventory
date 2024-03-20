ec68c2cee4bb92eda53b810478e7f80351299668
asofianidis <ansofianidis@outlook.com>
2024-03-19 21:12:24 -0500

    created util class to hash strings

---
+++ backend/Utils/HashString.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace backend.Utils
+{
+    public class HashString
+    {
+        public static string HashWithSHA256(string value)
+        {
+            using var hash = SHA256.Create();
+            var byteArray = hash.ComputeHash(Encoding.UTF8.GetBytes(value));
+            return Convert.ToHexString(byteArray);
+        }
+    }
+}
