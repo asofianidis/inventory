8d1d9b7a345e5b7a3dbcdaab393ab64997c18d09
asofianidis <ansofianidis@outlook.com>
2024-03-19 22:12:23 -0500

    wrote user login endpoint

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -35,9 +35,9 @@ namespace backend.Controllers
                 }
 
                 var rand = new Random();
-                var salt = HashString.HashWithSHA256(DateTime.Now.ToString() + rand.Next(100000, 1000000).ToString());
+                var salt = HashString.HashWithSHA256(DateTime.Now.ToString() + rand.Next(100000, 1000000).ToString()).ToLower();
 
-                var pw = HashString.HashWithSHA256(salt + user.password);
+                var pw = HashString.HashWithSHA256(salt + user.password).ToLower();
 
                 await using NpgsqlCommand insertUserCmd = new(@"INSERT INTO users (full_name, username, org_id, created, email, salt, pw) VALUES (@Full_name, @Username, @Org_id, NOW(), @Email, @Salt, @Pw) RETURNING user_id;", conn);
                 insertUserCmd.Parameters.AddWithValue("@Full_name", user.full_name);
@@ -58,6 +58,9 @@ namespace backend.Controllers
                 tokenCmd.Parameters.AddWithValue("@Token", HashString.HashWithSHA256(DateTime.Now.ToString() + rand.Next(100000, 1000000).ToString()));
                 var tokenResult = await tokenCmd.ExecuteScalarAsync();
 
+                await conn.CloseAsync();
+                NpgsqlConnection.ClearAllPools();
+
                 return Ok(tokenResult);
             }
             catch (Exception e)
@@ -67,6 +70,45 @@ namespace backend.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(UserLoginDTO loginInfo)
+        {
+            try
+            {
+                await using NpgsqlConnection conn = new(_connectionString);
+                await conn.OpenAsync();
+
+                await using NpgsqlCommand userCmd = new(@"SELECT user_id FROM users WHERE username=@Username AND LOWER(pw)=ENCODE(SHA256((salt || @Pw::text)::bytea), 'hex')", conn);
+                userCmd.Parameters.AddWithValue(@"Username", loginInfo.username);
+                userCmd.Parameters.AddWithValue("@Pw", loginInfo.password);
+                var result = await userCmd.ExecuteScalarAsync();
+
+                if (result == null) {
+                    throw new Exception("Couldn't find user id");
+                }
+
+                Random rand = new();
+
+                var token = HashString.HashWithSHA256(DateTime.Now.ToString() + rand.Next(100000, 1000000).ToString()).ToLower();
+
+                await using NpgsqlCommand tokenCmd = new(@"DELETE FROM users_tokens WHERE NOW() > expires OR user_id=@User_id;INSERT INTO users_tokens (user_id, token, created, expires) VALUES (@User_id, @Token, NOW(), NOW() + interval '30 days') RETURNING token", conn);
+                tokenCmd.Parameters.AddWithValue("@User_id", result);
+                tokenCmd.Parameters.AddWithValue("@Token", token);
+                var tokenResult = await tokenCmd.ExecuteScalarAsync();
+
+                if (tokenResult == null)
+                {
+                    throw new Exception("Failed token creation");
+                }
+
+
+                return Ok(tokenResult);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                return StatusCode(500);
+            }
+        }
 
     }
 }
---
+++ backend/DTO/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.DTO
+{
+    public class UserLoginDTO
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
